3a21c63da1addf53f457177dc83f4b49117872ee
Phan Van Minh <21522345@gm.uit.edu.vn>
2024-12-23 04:11:03 +0700

    Fix image cart

--- client/src/app/features/cart/cart-item/cart-item.component.ts
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -1,26 +1,22 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
 import { CartItem } from '../../../shared/models/cart';
 import { RouterLink } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { CurrencyPipe } from '@angular/common';
 import { CartService } from '../../../core/services/cart.service';
-
+import { environment } from '../../../../environments/environment';
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [
-    RouterLink,
-    MatButton,
-    MatIcon,
-    CurrencyPipe
-  ],
+  imports: [RouterLink, MatButton, MatIcon, CurrencyPipe],
   templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.scss'
+  styleUrl: './cart-item.component.scss',
 })
 export class CartItemComponent {
   item = input.required<CartItem>();
   cartService = inject(CartService);
+  baseUrl = environment.baseUrl;
 
   incrementQuantity() {
     this.cartService.addItemToCart(this.item());
@@ -31,6 +27,9 @@ export class CartItemComponent {
   }
 
   removeItemFromCart() {
-    this.cartService.removeItemFromCart(this.item().productId, this.item().quantity);
+    this.cartService.removeItemFromCart(
+      this.item().productId,
+      this.item().quantity
+    );
   }
 }
